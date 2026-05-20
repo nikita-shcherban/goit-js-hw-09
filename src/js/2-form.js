4f4f16c2de8100b735ace8bf2388592f422f54a9
Nikita Shcherban <cruizsports@gmail.com>
2026-05-20 17:34:36 +0300

    Complete tasks

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -8,17 +8,32 @@ const inputEmail = form.elements.email;
 const textarea = form.elements.message;
 const localStorageKey = 'feedback-form-state';
 
-inputEmail.value = localStorage.getItem(localStorageKey.formData.email) ?? '';
-textarea.value = localStorage.getItem(localStorageKey.formData.message) ?? '';
+const dataStorage = localStorage.getItem(localStorageKey);
+if (dataStorage) {
+  const dataStorageParse = JSON.parse(dataStorage);
+  inputEmail.value = dataStorageParse.email ?? '';
+  textarea.value = dataStorageParse.message ?? '';
+  formData.email = dataStorageParse.email ?? '';
+  formData.message = dataStorageParse.message ?? '';
+}
 
 form.addEventListener('input', event => {
-  formData.email = event.target.elements.email.value;
-  formData.message = event.target.elements.message.value;
-  localStorage.setItem(localStorageKey, event.target.value);
+  formData.email = inputEmail.value;
+  formData.message = textarea.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
 
 form.addEventListener('submit', event => {
   event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
   localStorage.removeItem(localStorageKey);
   form.reset();
+  formData.email = '';
+  formData.message = '';
 });
